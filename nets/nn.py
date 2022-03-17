e4c2c8b20d8f2b1bd7c55befa0efbc2ff415cffa
yakhyo <yakhyo9696@gmail.com>
2022-03-17 18:56:44 +0900

    Updates

--- nets/nn.py
+++ nets/nn.py
@@ -1,182 +1,160 @@
 import torch
-from torch import nn
+import torch.nn as nn
+from utils.utils import Conv2dAct, InvertedResidual, _make_divisible, _init_weight
 
-from utils.utils import Conv2dNormActivation, SqueezeExcitation, _make_divisible
 
+class MobileNetV3L(nn.Module):
+    _width = 1.0
 
-class InvertedResidual(nn.Module):
-    # Implemented as described at section 5 of MobileNetV3 paper
-    def __init__(self, in_channels, kernel, exp_channels, out_channels, use_se, activation, stride, dilation,
-                 width_mult):
+    def __init__(self, num_classes=1000, dropout=0.2, init_weight=True):
         super().__init__()
-        self.in_channels = _make_divisible(in_channels * width_mult)
-        self.exp_channels = _make_divisible(exp_channels * width_mult)
-        self.out_channels = _make_divisible(out_channels * width_mult)
+        if init_weight:
+            _init_weight(self)
 
-        self.use_res_connect = stride == 1 and self.in_channels == self.out_channels
+        _inp = [16, 16, 24, 24, 40, 40, 40, 80, 80, 80, 80, 112, 112, 160, 160, 160]
+        _mid = [16, 64, 72, 72, 120, 120, 240, 200, 184, 184, 480, 672, 672, 960, 960]
+        _out = [16, 24, 24, 40, 40, 40, 80, 80, 80, 80, 112, 112, 160, 160, 160, 960, 1280]
+        _last_io = _make_divisible(_out[-1] * self._width)
 
-        layers = []
-        activation_layer = nn.Hardswish if activation == 'HS' else nn.ReLU
+        self._layers = [Conv2dAct(3, _inp[0], 3, 2, act=nn.Hardswish)]
+        self._layers.extend([
+            InvertedResidual(_inp[0], _mid[0], _out[0], 3, 1, False, nn.ReLU),
+            InvertedResidual(_inp[1], _mid[1], _out[1], 3, 2, False, nn.ReLU),  # C1 1/2
+            InvertedResidual(_inp[2], _mid[2], _out[2], 3, 1, False, nn.ReLU),
 
-        # expand
-        if self.exp_channels != self.in_channels:
-            layers.append(Conv2dNormActivation(self.in_channels,
-                                               self.exp_channels,
-                                               kernel_size=1,
-                                               activation_layer=activation_layer)
-                          )
+            InvertedResidual(_inp[3], _mid[3], _out[3], 5, 2, True, nn.ReLU),  # C2 1/4
+            InvertedResidual(_inp[4], _mid[4], _out[4], 5, 1, True, nn.ReLU),
+            InvertedResidual(_inp[5], _mid[5], _out[5], 5, 1, True, nn.ReLU),
 
-        # depthwise
-        layers.append(Conv2dNormActivation(self.exp_channels,
-                                           self.exp_channels,
-                                           kernel_size=kernel,
-                                           stride=stride,
-                                           dilation=dilation,
-                                           groups=self.exp_channels,
-                                           activation_layer=activation_layer)
-                      )
-        if use_se:
-            squeeze_channels = _make_divisible(self.exp_channels // 4)
-            layers.append(SqueezeExcitation(self.exp_channels, squeeze_channels))
+            InvertedResidual(_inp[6], _mid[6], _out[6], 3, 2, False, nn.Hardswish),  # C3 1/8
+            InvertedResidual(_inp[7], _mid[7], _out[7], 3, 1, False, nn.Hardswish),
+            InvertedResidual(_inp[8], _mid[8], _out[8], 3, 1, False, nn.Hardswish),
+            InvertedResidual(_inp[9], _mid[9], _out[9], 3, 1, False, nn.Hardswish),
 
-        # project
-        layers.append(Conv2dNormActivation(self.exp_channels,
-                                           self.out_channels,
-                                           kernel_size=1,
-                                           activation_layer=None)
-                      )
+            InvertedResidual(_inp[10], _mid[10], _out[10], 3, 1, True, nn.Hardswish),
+            InvertedResidual(_inp[11], _mid[11], _out[11], 3, 1, True, nn.Hardswish),
 
-        self.block = nn.Sequential(*layers)
+            InvertedResidual(_inp[12], _mid[12], _out[12], 5, 2, True, nn.Hardswish),  # C4 1/16
+            InvertedResidual(_inp[13], _mid[13], _out[13], 5, 1, True, nn.Hardswish),
+            InvertedResidual(_inp[14], _mid[14], _out[14], 5, 1, True, nn.Hardswish),
+        ])
 
-    def forward(self, x):
-        result = self.block(x)
-        if self.use_res_connect:
-            result += x
-        return result
-
-
-class MobileNetV3(nn.Module):
-
-    def __init__(self, inverted_residual_setting, last_channel, num_classes=1000, dropout=0.2):
-        super().__init__()
-
-        layers = []
-
-        # building first layer
-        firstconv_output_channels = inverted_residual_setting[0].in_channels
-        layers.append(Conv2dNormActivation(3,
-                                           firstconv_output_channels,
-                                           kernel_size=3,
-                                           stride=2,
-                                           activation_layer=nn.Hardswish)
-                      )
-
-        # building inverted residual blocks
-        layers.extend(inverted_residual_setting)
-
-
-        # building last several layers
-        lastconv_input_channels = inverted_residual_setting[-1].out_channels
-        lastconv_output_channels = 6 * lastconv_input_channels
-
-        layers.append(Conv2dNormActivation(lastconv_input_channels,
-                                           lastconv_output_channels,
-                                           kernel_size=1,
-                                           activation_layer=nn.Hardswish)
-                      )
-
-        self.features = nn.Sequential(*layers)
-        self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.classifier = nn.Sequential(
-            nn.Linear(lastconv_output_channels, last_channel),
+        self._layers.append(Conv2dAct(_inp[15], _out[15], act=nn.Hardswish))
+        self._features = nn.Sequential(*self._layers)
+        self._pool = nn.AdaptiveAvgPool2d(1)
+        self._classifier = nn.Sequential(
+            nn.Linear(in_features=_out[15], out_features=_last_io),
             nn.Hardswish(inplace=True),
             nn.Dropout(p=dropout, inplace=True),
-            nn.Linear(last_channel, num_classes),
+            nn.Linear(in_features=_last_io, out_features=num_classes),
         )
 
-        self._init_weight()
-
-    def _init_weight(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode="fan_out")
-                if m.bias is not None:
-                    nn.init.zeros_(m.bias)
-            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
-                nn.init.ones_(m.weight)
-                nn.init.zeros_(m.bias)
-            elif isinstance(m, nn.Linear):
-                nn.init.normal_(m.weight, 0, 0.01)
-                nn.init.zeros_(m.bias)
+    def features(self, x):
+        return self._features(x)
 
     def forward(self, x):
-        x = self.features(x)
+        x = self._features(x)
 
-        x = self.avgpool(x)
+        x = self._pool(x)
         x = torch.flatten(x, 1)
 
-        x = self.classifier(x)
+        x = self._classifier(x)
 
         return x
 
 
-def _mobilenet_v3_conf(arch: str, width_mult: float = 1.0):
-    if arch == "mobilenet_v3_large":
-        inverted_residual_setting = [
-            InvertedResidual(16, 3, 16, 16, False, "RE", 1, 1, width_mult),
-            InvertedResidual(16, 3, 64, 24, False, "RE", 2, 1, width_mult),  # C1
-            InvertedResidual(24, 3, 72, 24, False, "RE", 1, 1, width_mult),
-            InvertedResidual(24, 5, 72, 40, True, "RE", 2, 1, width_mult),  # C2
-            InvertedResidual(40, 5, 120, 40, True, "RE", 1, 1, width_mult),
-            InvertedResidual(40, 5, 120, 40, True, "RE", 1, 1, width_mult),
-            InvertedResidual(40, 3, 240, 80, False, "HS", 2, 1, width_mult),  # C3
-            InvertedResidual(80, 3, 200, 80, False, "HS", 1, 1, width_mult),
-            InvertedResidual(80, 3, 184, 80, False, "HS", 1, 1, width_mult),
-            InvertedResidual(80, 3, 184, 80, False, "HS", 1, 1, width_mult),
-            InvertedResidual(80, 3, 480, 112, True, "HS", 1, 1, width_mult),
-            InvertedResidual(112, 3, 672, 112, True, "HS", 1, 1, width_mult),
-            InvertedResidual(112, 5, 672, 160, True, "HS", 2, 1, width_mult),  # C4
-            InvertedResidual(160, 5, 960, 160, True, "HS", 1, 1, width_mult),
-            InvertedResidual(160, 5, 960, 160, True, "HS", 1, 1, width_mult),
-        ]
-        last_channel = _make_divisible(1280 * width_mult)  # C5
-    elif arch == "mobilenet_v3_small":
-        inverted_residual_setting = [
-            InvertedResidual(16, 3, 16, 16, True, "RE", 2, 1, width_mult),  # C1
-            InvertedResidual(16, 3, 72, 24, False, "RE", 2, 1, width_mult),  # C2
-            InvertedResidual(24, 3, 88, 24, False, "RE", 1, 1, width_mult),
-            InvertedResidual(24, 5, 96, 40, True, "HS", 2, 1, width_mult),  # C3
-            InvertedResidual(40, 5, 240, 40, True, "HS", 1, 1, width_mult),
-            InvertedResidual(40, 5, 240, 40, True, "HS", 1, 1, width_mult),
-            InvertedResidual(40, 5, 120, 48, True, "HS", 1, 1, width_mult),
-            InvertedResidual(48, 5, 144, 48, True, "HS", 1, 1, width_mult),
-            InvertedResidual(48, 5, 288, 96, True, "HS", 2, 1, width_mult),  # C4
-            InvertedResidual(96, 5, 576, 96, True, "HS", 1, 1, width_mult),
-            InvertedResidual(96, 5, 576, 96, True, "HS", 1, 1, width_mult),
-        ]
-        last_channel = _make_divisible(1024 * width_mult)  # C5
-    else:
-        raise ValueError(f"Unsupported model type {arch}")
+class MobileNetV3S(nn.Module):
+    _width = 1.0
 
-    return inverted_residual_setting, last_channel
+    def __init__(self, num_classes=1000, dropout=0.2, init_weight=True):
+        super().__init__()
+        if init_weight:
+            _init_weight(self)
+
+        _inp = []
+        _mid = [16, 64, 72, 72, 120, 120, 240, 200, 184, 184, 480, 672, 672, 960, 960]
+        _out = [16, 24, 24, 40, 40, 40, 80, 80, 80, 80, 112, 112, 160, 160, 160, 960, 1280]
+        _last_io = _make_divisible(_out[-1] * self._width)
+
+        self._layers = [Conv2dAct(3, _inp[0], 3, 2, act=nn.Hardswish)]
+        self._layers.extend([
+            InvertedResidual(_inp[0], _mid[0], _out[0], 3, 1, False, nn.ReLU),
+            InvertedResidual(_inp[1], _mid[1], _out[1], 3, 2, False, nn.ReLU),  # C1 1/2
+            InvertedResidual(_inp[2], _mid[2], _out[2], 3, 1, False, nn.ReLU),
+
+            InvertedResidual(_inp[3], _mid[3], _out[3], 5, 2, True, nn.ReLU),  # C2 1/4
+            InvertedResidual(_inp[4], _mid[4], _out[4], 5, 1, True, nn.ReLU),
+            InvertedResidual(_inp[5], _mid[5], _out[5], 5, 1, True, nn.ReLU),
+
+            InvertedResidual(_inp[6], _mid[6], _out[6], 3, 2, False, nn.Hardswish),  # C3 1/8
+            InvertedResidual(_inp[7], _mid[7], _out[7], 3, 1, False, nn.Hardswish),
+            InvertedResidual(_inp[8], _mid[8], _out[8], 3, 1, False, nn.Hardswish),
+            InvertedResidual(_inp[9], _mid[9], _out[9], 3, 1, False, nn.Hardswish),
+
+            InvertedResidual(_inp[10], _mid[10], _out[10], 3, 1, True, nn.Hardswish),
+            InvertedResidual(_inp[11], _mid[11], _out[11], 3, 1, True, nn.Hardswish),
+
+            InvertedResidual(_inp[12], _mid[12], _out[12], 5, 2, True, nn.Hardswish),  # C4 1/16
+            InvertedResidual(_inp[13], _mid[13], _out[13], 5, 1, True, nn.Hardswish),
+            InvertedResidual(_inp[14], _mid[14], _out[14], 5, 1, True, nn.Hardswish),
+        ])
+
+        self._layers.append(Conv2dAct(_inp[15], _out[15], act=nn.Hardswish))
+        self._features = nn.Sequential(*self._layers)
+        self._pool = nn.AdaptiveAvgPool2d(1)
+        self._classifier = nn.Sequential(
+            nn.Linear(in_features=_out[15], out_features=_last_io),
+            nn.Hardswish(inplace=True),
+            nn.Dropout(p=dropout, inplace=True),
+            nn.Linear(in_features=_last_io, out_features=num_classes),
+        )
+
+    def features(self, x):
+        return self._features(x)
+
+    def forward(self, x):
+        x = self._features(x)
+
+        x = self._pool(x)
+        x = torch.flatten(x, 1)
+
+        x = self._classifier(x)
+
+        return x
 
 
-def _mobilenet_v3(inverted_residual_setting, last_channel):
-    return MobileNetV3(inverted_residual_setting, last_channel)
+#     elif arch == "mobilenet_v3_small":
+#         inverted_residual_setting = [
+#             InvertedResidual(16, 16, 16, 3, True, "RE", 2),  # C1
+#
+#             InvertedResidual(16, 72, 24, 3, False, "RE", 2),  # C2
+#             InvertedResidual(24, 88, 24, 3, False, "RE", 1),
+#
+#             InvertedResidual(24, 96, 40, 5, True, "HS", 2),  # C3
+#             InvertedResidual(40, 240, 40, 5, True, "HS", 1),
+#             InvertedResidual(40, 240, 40, 5, True, "HS", 1),
+#             InvertedResidual(40, 120, 48, 5, True, "HS", 1),
+#             InvertedResidual(48, 144, 48, 5, True, "HS", 1),
+#
+#             InvertedResidual(48, 288, 96, 5, True, "HS", 2),  # C4
+#             InvertedResidual(96, 576, 96, 5, True, "HS", 1),
+#             InvertedResidual(96, 576, 96, 5, True, "HS", 1),
+#         ]
+#         last_channel = _make_divisible(1024 * width_mult)  # C5
 
 
-def mobilenet_v3_large() -> MobileNetV3:
+def mobilenet_v3_large(**kwargs) -> MobileNetV3L:
     arch = "mobilenet_v3_large"
-    inverted_residual_setting, last_channel = _mobilenet_v3_conf(arch)
-    return _mobilenet_v3(inverted_residual_setting, last_channel)
+    return MobileNetV3L(**kwargs)
 
 
-def mobilenet_v3_small() -> MobileNetV3:
+def mobilenet_v3_small(**kwargs) -> MobileNetV3L:
     arch = "mobilenet_v3_small"
-    inverted_residual_setting, last_channel = _mobilenet_v3_conf(arch)
-    return _mobilenet_v3(inverted_residual_setting, last_channel)
+    return MobileNetV3L(**kwargs)
 
 
 if __name__ == '__main__':
-    model = mobilenet_v3_small()
+    model = mobilenet_v3_large()
+    img = torch.randn(1, 3, 224, 224)
+    print(model(img).shape)
     print("Num params. {}".format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
--- utils/utils.py
+++ utils/utils.py
@@ -1,23 +1,20 @@
 import torch
-import torch.nn as nn
 
 
-class Conv2dNormActivation(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=None, groups=1,
-                 activation_layer=torch.nn.ReLU, dilation=1, inplace=True):
+class Conv2dAct(torch.nn.Module):
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, d=1, g=1, act=None):
         super().__init__()
-        if padding is None:
-            padding = (kernel_size - 1) // 2 * dilation
-
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation=dilation, groups=groups,
-                              bias=False)
-        self.norm = nn.BatchNorm2d(out_channels, eps=0.001, momentum=0.01)
-
-        if activation_layer is not None:
-            params = {} if inplace is None else {"inplace": inplace}
-            self.act = activation_layer(**params)
-
-        self.out_channels = out_channels
+        self.conv = torch.nn.Conv2d(in_channels=c1,
+                                    out_channels=c2,
+                                    kernel_size=k,
+                                    stride=s,
+                                    padding=_pad(k) if p is None else p,
+                                    dilation=d,
+                                    groups=g,
+                                    bias=False)
+        self.norm = torch.nn.BatchNorm2d(num_features=c2, eps=0.001, momentum=0.01)
+        self.act = act(inplace=True) if act is not None else torch.nn.Identity()
 
     def forward(self, x):
         x = self.conv(x)
@@ -26,15 +23,16 @@ class Conv2dNormActivation(nn.Module):
         return x
 
 
-class SqueezeExcitation(nn.Module):
+class SqueezeExcitation(torch.nn.Module):
 
-    def __init__(self, c1, c2):
+    def __init__(self, c1):
         super().__init__()
-        self.pool = nn.AdaptiveAvgPool2d(1)
-        self.conv1 = nn.Conv2d(in_channels=c1, out_channels=c2, kernel_size=(1, 1))
-        self.conv2 = nn.Conv2d(in_channels=c2, out_channels=c1, kernel_size=(1, 1))
-        self.relu = nn.ReLU()
-        self.hard = nn.Hardsigmoid()
+        c2 = _make_divisible(c1 // 4)
+        self.pool = torch.nn.AdaptiveAvgPool2d(output_size=1)
+        self.conv1 = torch.nn.Conv2d(in_channels=c1, out_channels=c2, kernel_size=1)
+        self.conv2 = torch.nn.Conv2d(in_channels=c2, out_channels=c1, kernel_size=1)
+        self.relu = torch.nn.ReLU()
+        self.hard = torch.nn.Hardsigmoid()
 
     def _scale(self, x):
         x = self.pool(x)
@@ -48,9 +46,48 @@ class SqueezeExcitation(nn.Module):
         return x * self._scale(x)
 
 
+class InvertedResidual(torch.nn.Module):
+    _width_mult = 1.0
+
+    def __init__(self, in_channels, mid_channels, out_channels, kernel_size, stride, use_se, act):
+        super().__init__()
+        self._inp = _make_divisible(in_channels * self._width_mult)
+        self._mid = _make_divisible(mid_channels * self._width_mult)
+        self._out = _make_divisible(out_channels * self._width_mult)
+        self._shortcut = stride == 1 and self._inp == self._out
+
+        self._block = torch.nn.Sequential(
+            Conv2dAct(self._inp, self._mid, 1, act=act) if self._mid != self._inp else torch.nn.Identity(),
+            Conv2dAct(self._mid, self._mid, kernel_size, stride, g=self._mid, act=act),
+            SqueezeExcitation(self._mid) if use_se else torch.nn.Identity(),
+            Conv2dAct(self._mid, self._out, k=1, act=None),
+        )
+
+    def forward(self, x):
+        return x + self._block(x) if self._shortcut else self._block(x)
+
+
 def _make_divisible(width):
     divisor = 8
     new_width = max(divisor, int(width + divisor / 2) // divisor * divisor)
     if new_width < 0.9 * width:
         new_width += divisor
     return new_width
+
+
+def _pad(kernel_size, dilation=1):
+    return kernel_size // (2 * dilation)
+
+
+def _init_weight(self):
+    for m in self.modules():
+        if isinstance(m, torch.nn.Conv2d):
+            torch.nn.init.kaiming_normal_(m.weight, mode="fan_out")
+            if m.bias is not None:
+                torch.nn.init.zeros_(m.bias)
+        elif isinstance(m, (torch.nn.BatchNorm2d, torch.nn.GroupNorm)):
+            torch.nn.init.ones_(m.weight)
+            torch.nn.init.zeros_(m.bias)
+        elif isinstance(m, torch.nn.Linear):
+            torch.nn.init.normal_(m.weight, 0, 0.01)
+            torch.nn.init.zeros_(m.bias)
