4fd214b8a71fac45c35b0487fc2da2547f2b06b8
yakhyo <yakhyo9696@gmail.com>
2022-05-02 14:49:26 +0900

    tf rmsprop

--- nets/nn.py
+++ nets/nn.py
@@ -257,6 +257,77 @@ class CrossEntropyLoss:
         return F.cross_entropy(prediction, target, reduction=self.reduction, label_smoothing=self.label_smoothing)
 
 
+class RMSprop(torch.optim.Optimizer):
+
+    def __init__(self, params, lr=1e-2, alpha=0.9, eps=1e-10, weight_decay=0, momentum=0., centered=False,
+                 decoupled_decay=False, lr_in_momentum=True):
+
+        defaults = dict(lr=lr, momentum=momentum, alpha=alpha, eps=eps, centered=centered, weight_decay=weight_decay,
+                        decoupled_decay=decoupled_decay, lr_in_momentum=lr_in_momentum)
+        super(RMSprop, self).__init__(params, defaults)
+
+    def __setstate__(self, state):
+        super().__setstate__(state)
+        for group in self.param_groups:
+            group.setdefault('momentum', 0)
+            group.setdefault('centered', False)
+
+    def step(self, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('RMSprop does not support sparse gradients')
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['square_avg'] = torch.ones_like(p.data)  # PyTorch inits to zero
+                    if group['momentum'] > 0:
+                        state['momentum_buffer'] = torch.zeros_like(p.data)
+                    if group['centered']:
+                        state['grad_avg'] = torch.zeros_like(p.data)
+
+                square_avg = state['square_avg']
+                one_minus_alpha = 1. - group['alpha']
+
+                state['step'] += 1
+
+                if group['weight_decay'] != 0:
+                    if 'decoupled_decay' in group and group['decoupled_decay']:
+                        p.data.add_(p.data, alpha=-group['weight_decay'])
+                    else:
+                        grad = grad.add(p.data, alpha=group['weight_decay'])
+
+                square_avg.add_(grad.pow(2) - square_avg, alpha=one_minus_alpha)
+
+                if group['centered']:
+                    grad_avg = state['grad_avg']
+                    grad_avg.add_(grad - grad_avg, alpha=one_minus_alpha)
+                    avg = square_avg.addcmul(-1, grad_avg, grad_avg).add(group['eps']).sqrt_()
+                else:
+                    avg = square_avg.add(group['eps']).sqrt_()
+
+                if group['momentum'] > 0:
+                    buf = state['momentum_buffer']
+                    if 'lr_in_momentum' in group and group['lr_in_momentum']:
+                        buf.mul_(group['momentum']).addcdiv_(grad, avg, value=group['lr'])
+                        p.data.add_(-buf)
+                    else:
+                        buf.mul_(group['momentum']).addcdiv_(grad, avg)
+                        p.data.add_(buf, alpha=-group['lr'])
+                else:
+                    p.data.addcdiv_(grad, avg, value=-group['lr'])
+
+        return loss
+
+
 def mobilenet_v3_large(width_mult: float = 1.0, **kwargs) -> MobileNetV3L:
     """ MobileNet V3 Large """
     return MobileNetV3L(width_mult=width_mult, **kwargs)
