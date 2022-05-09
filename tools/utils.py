54bc44b05cc25aa78afcaf682c3606298b3cf768
yakhyo <yakhyo9696@gmail.com>
2022-05-09 18:10:40 +0900

    Update

--- tools/utils.py
+++ tools/utils.py
@@ -1,149 +1,7 @@
-import datetime
 import os
-import time
-from collections import defaultdict, deque
 
 import torch
 import torch.distributed as distributed
-
-
-class SmoothedValue:
-
-    def __init__(self, window_size=20, fmt=None):
-        if fmt is None:
-            fmt = "{median:.4f} ({global_avg:.4f})"
-        self.deque = deque(maxlen=window_size)
-        self.total = 0.0
-        self.count = 0
-        self.fmt = fmt
-
-    def update(self, value, n=1):
-        self.deque.append(value)
-        self.count += n
-        self.total += value * n
-
-    def synchronize_between_processes(self):
-        t = reduce_across_processes([self.count, self.total])
-        t = t.tolist()
-        self.count = int(t[0])
-        self.total = t[1]
-
-    @property
-    def median(self):
-        d = torch.tensor(list(self.deque))
-        return d.median().item()
-
-    @property
-    def avg(self):
-        d = torch.tensor(list(self.deque), dtype=torch.float32)
-        return d.mean().item()
-
-    @property
-    def global_avg(self):
-        return self.total / self.count
-
-    @property
-    def max(self):
-        return max(self.deque)
-
-    @property
-    def value(self):
-        return self.deque[-1]
-
-    def __str__(self):
-        return self.fmt.format(
-            median=self.median, avg=self.avg, global_avg=self.global_avg, max=self.max, value=self.value
-        )
-
-
-class MetricLogger:
-    def __init__(self, delimiter="\t"):
-        self.meters = defaultdict(SmoothedValue)
-        self.delimiter = delimiter
-
-    def update(self, **kwargs):
-        for k, v in kwargs.items():
-            if isinstance(v, torch.Tensor):
-                v = v.item()
-            assert isinstance(v, (float, int))
-            self.meters[k].update(v)
-
-    def __getattr__(self, attr):
-        if attr in self.meters:
-            return self.meters[attr]
-        if attr in self.__dict__:
-            return self.__dict__[attr]
-        raise AttributeError(f"'{type(self).__name__}' object has no attribute '{attr}'")
-
-    def __str__(self):
-        loss_str = []
-        for name, meter in self.meters.items():
-            loss_str.append(f"{name}: {str(meter)}")
-        return self.delimiter.join(loss_str)
-
-    def synchronize_between_processes(self):
-        for meter in self.meters.values():
-            meter.synchronize_between_processes()
-
-    def add_meter(self, name, meter):
-        self.meters[name] = meter
-
-    def log_every(self, iterable, print_freq, header=None):
-        i = 0
-        if not header:
-            header = ""
-        start_time = time.time()
-        end = time.time()
-        iter_time = SmoothedValue(fmt="{avg:.4f}")
-        data_time = SmoothedValue(fmt="{avg:.4f}")
-        space_fmt = ":" + str(len(str(len(iterable)))) + "d"
-        if torch.cuda.is_available():
-            log_msg = self.delimiter.join(
-                [
-                    header,
-                    "[{0" + space_fmt + "}/{1}]",
-                    "eta: {eta}",
-                    "{meters}",
-                    "time: {time}",
-                    "data: {data}",
-                    "max mem: {memory:.0f}",
-                ]
-            )
-        else:
-            log_msg = self.delimiter.join(
-                [header, "[{0" + space_fmt + "}/{1}]", "eta: {eta}", "{meters}", "time: {time}", "data: {data}"]
-            )
-        MB = 1024.0 * 1024.0
-        for obj in iterable:
-            data_time.update(time.time() - end)
-            yield obj
-            iter_time.update(time.time() - end)
-            if i % print_freq == 0:
-                eta_seconds = iter_time.global_avg * (len(iterable) - i)
-                eta_string = str(datetime.timedelta(seconds=int(eta_seconds)))
-                if torch.cuda.is_available():
-                    print(
-                        log_msg.format(
-                            i,
-                            len(iterable),
-                            eta=eta_string,
-                            meters=str(self),
-                            time=str(iter_time),
-                            data=str(data_time),
-                            memory=torch.cuda.max_memory_allocated() / MB,
-                        )
-                    )
-                else:
-                    print(
-                        log_msg.format(
-                            i, len(iterable), eta=eta_string, meters=str(self), time=str(iter_time), data=str(data_time)
-                        )
-                    )
-            i += 1
-            end = time.time()
-        total_time = time.time() - start_time
-        total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-        print(f"{header} Total time: {total_time_str}")
 
 
 def setup_for_distributed(is_master):
@@ -200,17 +58,6 @@ def init_distributed_mode(args):
     setup_for_distributed(args.rank == 0)
 
 
-def reduce_across_processes(val):
-    if not is_dist_avail_and_initialized():
-        # nothing to sync, but we still convert to tensor for consistency with the distributed case.
-        return torch.tensor(val)
-
-    t = torch.tensor(val, device="cuda")
-    distributed.barrier()
-    distributed.all_reduce(t)
-    return t
-
-
 def accuracy(output, target, topk):
     with torch.inference_mode():
         maxk = max(topk)
@@ -239,5 +86,30 @@ def add_weight_decay(net, weight_decay=1e-5):
             no_decay.append(param)
         else:
             decay.append(param)
-    return [{'params': no_decay, 'weight_decay': 0.},
-            {'params': decay, 'weight_decay': weight_decay}]
+    return [{'params': no_decay, 'weight_decay': 0.}, {'params': decay, 'weight_decay': weight_decay}]
+
+
+class AverageMeter:
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def reduce_tensor(tensor, n):
+    rt = tensor.clone()
+    distributed.all_reduce(rt, op=distributed.ReduceOp.SUM)
+    rt /= n
+    return rt
--- train.py
+++ train.py
@@ -5,23 +5,25 @@ import time
 import torch
 import torch.utils.data
 
-from torch import optim
 from torchvision.transforms.functional import InterpolationMode
 
 from nets import nn
 from torchvision.transforms import transforms, autoaugment
 
 from tools import utils, dataset
+from tools.utils import AverageMeter
 
 
 def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, args, model_ema=None):
     model.train()
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    metric_logger.add_meter("lr", utils.SmoothedValue(window_size=1, fmt="{value}"))
-    metric_logger.add_meter("img/s", utils.SmoothedValue(window_size=10, fmt="{value}"))
 
-    header = f"Epoch: [{epoch}]"
-    for i, (image, target) in enumerate(metric_logger.log_every(data_loader, args.print_freq, header)):
+    batch_time_m = AverageMeter()
+    losses_m = AverageMeter()
+    lr_m = AverageMeter()
+    top1_m = AverageMeter()
+    top5_m = AverageMeter()
+
+    for batch_idx, (image, target) in enumerate(data_loader):
         start_time = time.time()
         image, target = image.to(device), target.to(device)
         output = model(image)
@@ -35,71 +37,116 @@ def train_one_epoch(model, criterion, optimizer, data_loader, device, epoch, arg
 
         acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
         batch_size = image.shape[0]
-        metric_logger.update(loss=loss.item(), lr=optimizer.param_groups[0]["lr"])
-        metric_logger.meters["acc1"].update(acc1.item(), n=batch_size)
-        metric_logger.meters["acc5"].update(acc5.item(), n=batch_size)
-        metric_logger.meters["img/s"].update(batch_size / (time.time() - start_time))
+
+        if args.distributed:
+            reduced_loss = utils.reduce_tensor(loss.data, args.world_size)
+            acc1 = utils.reduce_tensor(acc1, args.world_size)
+            acc5 = utils.reduce_tensor(acc5, args.world_size)
+        else:
+            reduced_loss = loss.data
+
+        torch.cuda.synchronize()
+
+        losses_m.update(reduced_loss.item(), batch_size)
+        top1_m.update(acc1.item(), batch_size)
+        top5_m.update(acc5.item(), batch_size)
+        batch_time_m.update(batch_size / (time.time() - start_time))
+        lr_m.update(optimizer.param_groups[0]['lr'])
+
+        if args.rank == 0 and batch_idx % args.print_freq == 0:
+            lrl = [param_group['lr'] for param_group in optimizer.param_groups]
+            lr = sum(lrl) / len(lrl)
+            print('Epoch: [{epoch}][{batch_idx}/{len_loader}] '
+                  'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})  '
+                  'LR: {lr_m:.3e} '
+                  'Loss: {loss.val:>7.4f} ({loss.avg:>6.4f})  '
+                  'Acc@1: {top1.val:>7.4f} ({top1.avg:>7.4f})  '
+                  'Acc@5: {top5.val:>7.4f} ({top5.avg:>7.4f})'.format(
+                epoch=epoch, batch_idx=batch_idx, len_loader=len(data_loader), lr_m=lr,
+                batch_time=batch_time_m, loss=losses_m, top1=top1_m, top5=top5_m))
 
 
 def evaluate(model, criterion, train_loader, device, print_freq=100, log_suffix=""):
-    model.eval()
-    metric_logger = utils.MetricLogger(delimiter="  ")
-    header = f"Test: {log_suffix}"
+    batch_time_m = AverageMeter()
+    losses_m = AverageMeter()
+    top1_m = AverageMeter()
+    top5_m = AverageMeter()
 
+    end = time.time()
+    last_idx = len(train_loader) - 1
     with torch.inference_mode():
-        for image, target in metric_logger.log_every(train_loader, print_freq, header):
+        for batch_idx, (image, target) in enumerate(train_loader):
             image = image.to(device, non_blocking=True)
             target = target.to(device, non_blocking=True)
             output = model(image)
             loss = criterion(output, target)
-
             acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
-
             batch_size = image.shape[0]
-            metric_logger.update(loss=loss.item())
-            metric_logger.meters["acc1"].update(acc1.item(), n=batch_size)
-            metric_logger.meters["acc5"].update(acc5.item(), n=batch_size)
 
-    # gather the stats from all processes
-    metric_logger.synchronize_between_processes()
-    print(f"{header} Acc@1 {metric_logger.acc1.global_avg:.3f} Acc@5 {metric_logger.acc5.global_avg:.3f}")
+            if args.distributed:
+                reduced_loss = utils.reduce_tensor(loss.data, args.world_size)
+                acc1 = utils.reduce_tensor(acc1, args.world_size)
+                acc5 = utils.reduce_tensor(acc5, args.world_size)
+            else:
+                reduced_loss = loss.data
 
-    return metric_logger.acc1.global_avg, acc1, acc5
+            torch.cuda.synchronize()
+
+            batch_time_m.update(time.time() - end)
+            losses_m.update(reduced_loss.item(), batch_size)
+            top1_m.update(acc1.item(), batch_size)
+            top5_m.update(acc5.item(), batch_size)
+
+            end = time.time()
+            if args.rank == 0 and batch_idx % print_freq == 0:
+                log_name = 'Test' + log_suffix
+                print('{0}: [{1:>4d}/{2}]  '
+                      'Time: {batch_time.val:.3f} ({batch_time.avg:.3f})  '
+                      'Loss: {loss.val:>7.4f} ({loss.avg:>6.4f})  '
+                      'Acc@1: {top1.val:>7.4f} ({top1.avg:>7.4f})  '
+                      'Acc@5: {top5.val:>7.4f} ({top5.avg:>7.4f})'.format(log_name,
+                                                                          batch_idx,
+                                                                          last_idx,
+                                                                          batch_time=batch_time_m,
+                                                                          loss=losses_m,
+                                                                          top1=top1_m,
+                                                                          top5=top5_m))
+
+    # metrics = OrderedDict([('loss', losses_m.avg), ('top1', top1_m.avg), ('top5', top5_m.avg)])
+
+    return losses_m.avg, top1_m.avg, top5_m.avg
 
 
-def load_data(traindir, valdir, args):
-    # Data loading code
-    print("Loading data")
-
-    print("Loading training data")
+def load_data(args):
+    print('Loading Data')
+    print('Loading Training Data')
     st = time.time()
     train_dataset = dataset.ImageFolder(
-        traindir,
+        os.path.join(args.data_path, "train"),
         transform=transforms.Compose([
-            transforms.RandomResizedCrop(args.train_crop_size, interpolation=InterpolationMode(args.interpolation)),
+            transforms.RandomResizedCrop(size=224, interpolation=InterpolationMode.BILINEAR),
             transforms.RandomHorizontalFlip(0.5),
-            autoaugment.AutoAugment(interpolation=InterpolationMode(args.interpolation)),
+            autoaugment.AutoAugment(interpolation=InterpolationMode.BILINEAR),
             transforms.PILToTensor(),
             transforms.ConvertImageDtype(torch.float),
             transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
             transforms.RandomErasing(args.random_erase),
         ])
     )
-    print("Took", time.time() - st)
+    print(f'Took {time.time() - st}')
 
-    print("Loading validation data")
+    print('Loading Validation Data')
     test_dataset = dataset.ImageFolder(
-        valdir,
+        os.path.join(args.data_path, "val"),
         transform=transforms.Compose([
-            transforms.Resize(args.val_resize_size, interpolation=InterpolationMode(args.interpolation)),
-            transforms.CenterCrop(args.val_crop_size),
+            transforms.Resize(size=256, interpolation=InterpolationMode.BILINEAR),
+            transforms.CenterCrop(size=224),
             transforms.PILToTensor(),
             transforms.ConvertImageDtype(torch.float),
             transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ])
     )
 
-    print("Creating data loaders")
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
         test_sampler = torch.utils.data.distributed.DistributedSampler(test_dataset, shuffle=False)
@@ -107,48 +154,39 @@ def load_data(traindir, valdir, args):
         train_sampler = torch.utils.data.RandomSampler(train_dataset)
         test_sampler = torch.utils.data.SequentialSampler(test_dataset)
 
-    # Prepare data loaders
-    train_loader = torch.utils.data.DataLoader(
-        train_dataset,
-        batch_size=args.batch_size,
-        sampler=train_sampler,
-        num_workers=args.workers,
-        pin_memory=True,
-    )
-    test_loader = torch.utils.data.DataLoader(
-        test_dataset, batch_size=args.batch_size, sampler=test_sampler, num_workers=args.workers, pin_memory=True
-    )
-
-    return train_dataset, test_dataset, train_loader, test_loader, train_sampler, test_sampler
+    return train_dataset, test_dataset, train_sampler, test_sampler
 
 
 def main(args):
     utils.init_distributed_mode(args)
     print(args)
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if not torch.cuda.is_available():
-        raise RuntimeError("COULD NOT FOUND GPU MACHINE")
-
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     torch.backends.cudnn.benchmark = True
 
-    train_dir = os.path.join(args.data_path, "train")
-    val_dir = os.path.join(args.data_path, "val")
-    train_dataset, test_dataset, train_loader, test_loader, train_sampler, test_sampler = load_data(train_dir, val_dir,
-                                                                                                    args)
+    train_dataset, test_dataset, train_sampler, test_sampler = load_data(args)
 
-    num_classes = len(train_dataset.classes)
+    print('Creating Data Loaders')
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=args.batch_size,
+                                               sampler=train_sampler,
+                                               num_workers=args.workers,
+                                               pin_memory=True)
 
-    print("Creating model")
-    model = nn.MobileNetV3L(num_classes=num_classes, width_mult=1.0)
-    model.to(device)
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                              batch_size=args.batch_size,
+                                              sampler=test_sampler,
+                                              num_workers=args.workers,
+                                              pin_memory=True)
 
+    print('Creating Model')
+    model = nn.MobileNetV3L(width_mult=1.0).to(device)
     if args.distributed and args.sync_bn:
         model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
 
-    parameters = utils.add_weight_decay(model)
-    criterion = nn.PolyLoss(label_smoothing=args.label_smoothing)
-    optimizer = optim.RMSprop(parameters, lr=args.lr, momentum=args.momentum, weight_decay=1e-5, eps=0.0316, alpha=0.9)
+    parameters = utils.add_weight_decay(model, weight_decay=args.weight_decay)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = nn.RMSprop(parameters, lr=args.lr, alpha=0.9, eps=1e-3, weight_decay=0, momentum=args.momentum)
     scheduler = nn.StepLR(optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma)
 
     model_without_ddp = model
@@ -166,32 +204,34 @@ def main(args):
         args.start_epoch = checkpoint["epoch"] + 1
         model_ema.model.load_state_dict(checkpoint["model_ema"])
 
-    print("Start training")
+    print("Start Training")
     start_time = time.time()
     best = 0
     for epoch in range(args.start_epoch, args.epochs):
         if args.distributed:
             train_sampler.set_epoch(epoch)
-        train_one_epoch(model, criterion, optimizer, train_loader, device, epoch, args, model_ema)
-        scheduler.step()
-        evaluate(model, criterion, test_loader, device=device)
-        _, acc1, acc5 = evaluate(model_ema.model, criterion, test_loader, device=device, log_suffix='EMA')
-        checkpoint = {"model": model_without_ddp.state_dict(),
-                      "model_ema": model_ema.model.state_dict(),
-                      "optimizer": optimizer.state_dict(),
-                      "scheduler": scheduler.state_dict(),
-                      "epoch": epoch,
-                      "args": args,
-                      }
 
-        torch.save(checkpoint, "weights/last.pth")
+        train_one_epoch(model, criterion, optimizer, train_loader, device, epoch, args, model_ema)
+        scheduler.step(epoch + 1)
+        # acc1, acc5 = evaluate(model, criterion, test_loader, device=device)
+        _, acc1, acc5 = evaluate(model_ema.model, criterion, test_loader, device=device, log_suffix='EMA')
+        checkpoint = {
+            'model': model_without_ddp.state_dict(),
+            'model_ema': model_ema.model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'scheduler': scheduler.state_dict(),
+            'epoch': epoch,
+            'args': args,
+        }
+
+        torch.save(checkpoint, 'weights/last.pth')
         if acc1 > best:
-            torch.save(checkpoint, "weights/best.pth")
+            torch.save(checkpoint, 'weights/best.pth')
         best = max(acc1, best)
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-    print(f"Training time {total_time_str}")
+    print(f"Training Time {total_time_str}")
 
 
 def get_args_parser():
@@ -209,7 +249,6 @@ def get_args_parser():
     parser.add_argument("--momentum", default=0.9, type=float, help="momentum")
     parser.add_argument("--weight-decay", default=1e-4, type=float, help="weight decay")
 
-    parser.add_argument("--label-smoothing", default=0.0, type=float, help="label smoothing (default: 0.0)")
     parser.add_argument("--lr-step-size", default=30, type=int, help="decrease lr every step-size epochs")
     parser.add_argument("--lr-gamma", default=0.1, type=float, help="decrease lr by a factor of lr-gamma")
 
@@ -218,18 +257,11 @@ def get_args_parser():
     parser.add_argument("--start-epoch", default=0, type=int, help="start epoch")
 
     parser.add_argument("--sync-bn", help="Use sync batch norm", action="store_true")
-    parser.add_argument("--auto-augment", default=None, type=str, help="auto augment policy")
     parser.add_argument("--random-erase", default=0.0, type=float, help="random erasing probability")
 
     # distributed training parameters
     parser.add_argument("--world-size", default=1, type=int, help="number of distributed processes")
     parser.add_argument("--dist-url", default="env://", type=str, help="url used to set up distributed training")
-
-    # dataset resize
-    parser.add_argument("--interpolation", default="bilinear", type=str, help="the interpolation method")
-    parser.add_argument("--val-resize-size", default=256, type=int, help="the resize size used for validation")
-    parser.add_argument("--val-crop-size", default=224, type=int, help="the central crop size used for validation")
-    parser.add_argument("--train-crop-size", default=224, type=int, help="the random crop size used for training")
 
     return parser
 
