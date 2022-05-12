ee54653518621a4b0ceb53dbc3ce5fc16f7fad45
yakhyo <yakhyo9696@gmail.com>
2022-05-12 09:56:24 +0900

    Update utils.py

--- tools/utils.py
+++ tools/utils.py
@@ -4,77 +4,46 @@ import torch
 import torch.distributed as distributed
 
 
-def setup_for_distributed(is_master):
-    """
-    This function disables printing when not in master process
-    """
-    import builtins as __builtin__
+class AverageMeter:
+    """Computes and stores the average and current value"""
 
-    builtin_print = __builtin__.print
+    def __init__(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
 
-    def print(*args, **kwargs):
-        force = kwargs.pop("force", False)
-        if is_master or force:
-            builtin_print(*args, **kwargs)
-
-    __builtin__.print = print
-
-
-def is_dist_avail_and_initialized():
-    if not distributed.is_available():
-        return False
-    if not distributed.is_initialized():
-        return False
-    return True
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
-def init_distributed_mode(args):
-    print('RANK' in os.environ)
-    print('SLURM_PROCID' in os.environ)
-    print(hasattr(args, 'rank'))
-    if "RANK" in os.environ and "WORLD_SIZE" in os.environ:
-        args.rank = int(os.environ["RANK"])
-        args.world_size = int(os.environ["WORLD_SIZE"])
-        args.gpu = int(os.environ["LOCAL_RANK"])
-    elif "SLURM_PROCID" in os.environ:
-        args.rank = int(os.environ["SLURM_PROCID"])
-        args.gpu = args.rank % torch.cuda.device_count()
-    elif hasattr(args, "rank"):
-        pass
-    else:
-        print("Not using distributed mode")
-        args.distributed = False
-        return
-
-    args.distributed = True
-
-    torch.cuda.set_device(args.gpu)
-    args.dist_backend = "nccl"
-    print(f"| distributed init (rank {args.rank}): {args.dist_url}", flush=True)
-    torch.distributed.init_process_group(
-        backend=args.dist_backend, init_method=args.dist_url, world_size=args.world_size, rank=args.rank
-    )
-    torch.distributed.barrier()
-    setup_for_distributed(args.rank == 0)
-
-
-def accuracy(output, target, topk=(1,)):
+def accuracy(output, target, top_k):
     """Computes the accuracy over the k top predictions for the specified values of k"""
     with torch.inference_mode():
-        maxk = max(topk)
+        max_k = max(top_k)
         batch_size = target.size(0)
         if target.ndim == 2:
             target = target.max(dim=1)[1]
 
-        _, pred = output.topk(maxk, 1, True, True)
+        _, pred = output.topk(max_k, 1, True, True)
         pred = pred.t()
         correct = pred.eq(target[None])
 
         res = []
-        for k in topk:
+        for k in top_k:
             correct_k = correct[:k].flatten().sum(dtype=torch.float32)
             res.append(correct_k * (100.0 / batch_size))
         return res
+
+
+def reduce_tensor(tensor, n):
+    rt = tensor.clone()
+    distributed.all_reduce(rt, op=distributed.ReduceOp.SUM)
+    rt /= n
+    return rt
 
 
 def add_weight_decay(net, weight_decay=1e-5):
@@ -90,27 +59,30 @@ def add_weight_decay(net, weight_decay=1e-5):
     return [{'params': no_decay, 'weight_decay': 0.}, {'params': decay, 'weight_decay': weight_decay}]
 
 
-class AverageMeter:
-    """Computes and stores the average and current value"""
+def init_distributed_mode(args):
+    args.distributed = int(os.getenv('WORLD_SIZE', 1)) > 1
+    if args.distributed:
+        args.local_rank = int(os.environ["RANK"])
+        args.world_size = int(os.environ["WORLD_SIZE"])
+        torch.cuda.set_device(args.local_rank)
+        torch.distributed.init_process_group(backend='nccl', init_method='env://')
 
-    def __init__(self):
-        self.reset()
+    print(f"| distributed init (rank {args.local_rank}): env://", flush=True)
 
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
+    setup_for_distributed(args.local_rank == 0)
 
 
-def reduce_tensor(tensor, n):
-    rt = tensor.clone()
-    distributed.all_reduce(rt, op=distributed.ReduceOp.SUM)
-    rt /= n
-    return rt
+def setup_for_distributed(is_master):
+    """
+    This function disables printing when not in master process
+    """
+    import builtins as __builtin__
+
+    builtin_print = __builtin__.print
+
+    def print(*args, **kwargs):
+        force = kwargs.pop("force", False)
+        if is_master or force:
+            builtin_print(*args, **kwargs)
+
+    __builtin__.print = print
